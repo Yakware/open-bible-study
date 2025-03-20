6470e1d8fe67624ced629248a639abb95ddd09f4
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-19 20:47:43 -0700

    update header for auth

--- src/modules/common/components/header/index.tsx
+++ src/modules/common/components/header/index.tsx
@@ -1,13 +1,32 @@
+"use client";
 import { APP_NAME } from "@/lib/constants";
 import Link from "next/link";
+import { login, logout } from "@/modules/auth/actions";
+import { Button } from "../ui/button";
+import { useIsAuthenticated } from "@/lib/context/auth-context";
 
 export function Header() {
+  const isAuthenticated = useIsAuthenticated();
+
   return (
-    <header>
-      <div>
-        <Link href="/">{APP_NAME}</Link>
+    <header className="bg-white">
+      <div className="container mx-auto py-4 flex justify-between items-center">
+        <h1 className="text-2xl font-semibold">
+          <Link href="/">{APP_NAME}</Link>
+        </h1>
+
+        <nav>
+          <ul>
+            <li>
+              {isAuthenticated ? (
+                <Button onClick={logout}>Logout</Button>
+              ) : (
+                <Button onClick={login}>Login</Button>
+              )}
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav></nav>
     </header>
   );
 }
