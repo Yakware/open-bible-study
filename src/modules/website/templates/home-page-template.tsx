668039619d449f3fc3776620f7a1afd6a1fa48a8
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-19 20:55:02 -0700

    make home page take up fullwidth of its parent container

--- src/modules/website/templates/home-page-template.tsx
+++ src/modules/website/templates/home-page-template.tsx
@@ -2,8 +2,8 @@ import { BookIcon } from "@/modules/common/icons/book-icon";
 
 export function HomePageTemplate() {
   return (
-    <>
+    <div className="flex-1">
       <BookIcon />
-    </>
+    </div>
   );
 }
