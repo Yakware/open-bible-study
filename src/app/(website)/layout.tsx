a775fbe5a547bd5fac8f3de04f261c3d588e9ef9
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-19 20:54:48 -0700

    update website layout

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { geistMono, geistSans } from "../fonts";
 import { Header } from "@/modules/common/components/header";
+import { Footer } from "@/modules/common/components/footer";
 
 type WebsiteLayoutProps = {
   children?: ReactNode;
@@ -8,9 +9,12 @@ type WebsiteLayoutProps = {
 
 export default function WebsiteLayout({ children }: WebsiteLayoutProps) {
   return (
-    <div className={`${geistSans.variable} ${geistMono.variable}`}>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen`}
+    >
       <Header />
       {children}
+      <Footer />
     </div>
   );
 }
