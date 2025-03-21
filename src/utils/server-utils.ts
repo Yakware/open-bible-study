35c22a5f2689dbeb4244da5c6be05eda4b51da76
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-20 18:28:27 -0700

    throw error when user is not authenticated

--- src/utils/server-utils.ts
+++ src/utils/server-utils.ts
@@ -3,7 +3,12 @@
 import { logtoConfig } from "@/app/logto";
 import { getLogtoContext } from "@logto/next/server-actions";
 
-export async function isAuthenticated() {
+export async function checkAuthenticated() {
   const { isAuthenticated } = await getLogtoContext(logtoConfig);
+
+  if (!isAuthenticated) {
+    throw new Error("Not authenticated");
+  }
+
   return isAuthenticated;
 }
