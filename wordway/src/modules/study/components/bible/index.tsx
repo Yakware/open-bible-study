4d19dc8b2a437ae38a66ad01d2d47652fb5d6dfa
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-22 16:22:35 -0700

    update study page design

--- wordway/src/modules/study/components/bible/index.tsx
+++ wordway/src/modules/study/components/bible/index.tsx
@@ -1,13 +1,23 @@
 import { useVerses } from "@/lib/context/study-context";
 import { NoVerseSelected } from "./no-verse-selected";
+import { useChapterNumber } from "../bible-navigation/hooks/use-chapter-number";
+import { useBookName } from "../bible-navigation/hooks/use-book-name";
 
 export function Bible() {
   const verses = useVerses();
+  const [bookName] = useBookName();
+  const [chapterNumber] = useChapterNumber();
 
   return (
     <div>
       {verses.length > 0 ? (
         <div className="font-reading flex flex-col gap-2">
+          <div className="flex flex-col font-sans justify-center items-center">
+            <p className="text-3xl font-bold">
+              {bookName} {chapterNumber}
+            </p>
+          </div>
+
           {verses.map((verse) => (
             <p key={verse.id}>
               <span className="align-super mr-1 ml-2">{verse.number}</span>
