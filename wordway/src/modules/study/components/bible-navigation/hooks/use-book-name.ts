5bc5e5825e892b4d6ad7de28aa35db27e80c977c
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-22 15:32:53 -0700

    use push instead of replace to preserve history

--- wordway/src/modules/study/components/bible-navigation/hooks/use-book-name.ts
+++ wordway/src/modules/study/components/bible-navigation/hooks/use-book-name.ts
@@ -3,6 +3,7 @@ import { useQueryState } from "nuqs";
 export function useBookName() {
   const queryState = useQueryState("book", {
     shallow: false,
+    history: "push",
   });
 
   return queryState;
--- wordway/src/modules/study/components/bible-navigation/hooks/use-version-name.ts
+++ wordway/src/modules/study/components/bible-navigation/hooks/use-version-name.ts
@@ -3,6 +3,7 @@ import { useQueryState } from "nuqs";
 export function useVersionName() {
   const queryState = useQueryState("version", {
     shallow: false,
+    history: "push",
   });
 
   return queryState;
