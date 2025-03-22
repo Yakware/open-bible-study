ef962565ede8cb688f285fb50061fee5502dc7e4
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-22 15:08:34 -0700

    add no verse selected view

--- wordway/src/modules/study/components/bible/index.tsx
+++ wordway/src/modules/study/components/bible/index.tsx
@@ -1,16 +1,21 @@
 import { useVerses } from "@/lib/context/study-context";
+import { NoVerseSelected } from "./no-verse-selected";
 
 export function Bible() {
   const verses = useVerses();
 
   return (
     <div className="flex flex-col gap-2">
-      {verses.map((verse) => (
-        <p key={verse.id}>
-          <span className="align-super mr-1 ml-2">{verse.number}</span>
-          {verse.text}
-        </p>
-      ))}
+      {verses.length > 0 ? (
+        verses.map((verse) => (
+          <p key={verse.id}>
+            <span className="align-super mr-1 ml-2">{verse.number}</span>
+            {verse.text}
+          </p>
+        ))
+      ) : (
+        <NoVerseSelected />
+      )}
     </div>
   );
 }
