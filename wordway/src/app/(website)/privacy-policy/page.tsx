cd65532c1ed54f7c2f3679de24dd6c2574f26ce7
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-23 14:31:55 -0700

    add privacy policy

---
+++ wordway/src/app/(website)/privacy-policy/page.tsx
@@ -0,0 +1,5 @@
+import { PrivacyPolicyTemplate } from "@/modules/website/templates/privacy-policy-template";
+
+export default function PrivacyPolicy() {
+  return <PrivacyPolicyTemplate />;
+}
--- wordway/src/modules/common/components/footer/index.tsx
+++ wordway/src/modules/common/components/footer/index.tsx
@@ -1,13 +1,15 @@
 import { APP_NAME } from "@/lib/constants";
+import Link from "next/link";
 
 export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex items-center justify-center">
-      <p className="py-2">
+    <footer className="flex flex-col items-center justify-center gap-1 py-2">
+      <p>
         &copy; {currentYear} {APP_NAME}. All rights reserved.
       </p>
+      <Link href="/privacy-policy">Privacy Policy</Link>
     </footer>
   );
 }
