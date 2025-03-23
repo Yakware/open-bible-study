bd809294842aa2308e327f039f73854e2e57bcd5
David Botos <dbotos@umich.edu>
2025-03-22 21:09:46 -0700

    bible base init

---
+++ biblebase/target/debug/deps/biblebase-753f24abf3d41f72.d
@@ -0,0 +1,5 @@
+/Users/davidbotos/Desktop/10-Faith/wordway/biblebase/target/debug/deps/libbiblebase-753f24abf3d41f72.rmeta: src/main.rs
+
+/Users/davidbotos/Desktop/10-Faith/wordway/biblebase/target/debug/deps/biblebase-753f24abf3d41f72.d: src/main.rs
+
+src/main.rs:
---
+++ biblebase/target/debug/deps/biblebase-d1dcd161d00bed4f.d
@@ -0,0 +1,5 @@
+/Users/davidbotos/Desktop/10-Faith/wordway/biblebase/target/debug/deps/libbiblebase-d1dcd161d00bed4f.rmeta: src/main.rs
+
+/Users/davidbotos/Desktop/10-Faith/wordway/biblebase/target/debug/deps/biblebase-d1dcd161d00bed4f.d: src/main.rs
+
+src/main.rs:
