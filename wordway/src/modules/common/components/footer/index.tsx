55432d3bc6e724e739c19a1983f599177d0ccca1
Chris Short <13677134+devcshort@users.noreply.github.com>
2025-03-23 14:40:24 -0700

    Feat/privacy (#10)
    * add privacy policy
    
    * add react cookie consent
    
    * add cookie consent button

--- wordway/src/modules/common/components/footer/index.tsx
+++ wordway/src/modules/common/components/footer/index.tsx
@@ -1,13 +1,25 @@
+"use client";
 import { APP_NAME } from "@/lib/constants";
+import Link from "next/link";
+import { CookieConsent } from "react-cookie-consent";
 
 export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex items-center justify-center">
-      <p className="py-2">
+    <footer className="flex flex-col items-center justify-center gap-1 py-2">
+      <p>
         &copy; {currentYear} {APP_NAME}. All rights reserved.
       </p>
+      <Link href="/privacy-policy">Privacy Policy</Link>
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="cookie-consent"
+        buttonStyle={{ borderRadius: "16px" }}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
     </footer>
   );
 }
