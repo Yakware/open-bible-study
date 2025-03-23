d519b157f7d3fc418c2863134cbdc690098de182
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-23 14:39:50 -0700

    add cookie consent button

--- wordway/src/modules/common/components/footer/index.tsx
+++ wordway/src/modules/common/components/footer/index.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { APP_NAME } from "@/lib/constants";
 import Link from "next/link";
+import { CookieConsent } from "react-cookie-consent";
 
 export function Footer() {
   const currentYear = new Date().getFullYear();
@@ -10,6 +12,14 @@ export function Footer() {
         &copy; {currentYear} {APP_NAME}. All rights reserved.
       </p>
       <Link href="/privacy-policy">Privacy Policy</Link>
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="cookie-consent"
+        buttonStyle={{ borderRadius: "16px" }}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
     </footer>
   );
 }
