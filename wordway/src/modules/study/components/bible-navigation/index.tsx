1b9d7f5edf539d749b0d3207ecc20f7d5dbdeef0
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-22 21:17:30 -0700

    add abiilty to reset back to first view

--- wordway/src/modules/study/components/bible-navigation/index.tsx
+++ wordway/src/modules/study/components/bible-navigation/index.tsx
@@ -5,15 +5,19 @@ import {
 import { VersionNavigation } from "./version-navigation";
 import { BookNavigation } from "./book-navigation";
 import { ChapterNavigation } from "./chapter-navigation";
+import { HomeButton } from "./home-button";
 
 export function BibleNavigation() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <VersionNavigation />
-        <BookNavigation />
-        <ChapterNavigation />
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="flex items-center gap-1">
+      <HomeButton />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <VersionNavigation />
+          <BookNavigation />
+          <ChapterNavigation />
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 }
