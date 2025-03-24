f92e6d042d9f61a802638fd6d93c2c87da04e574
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-23 17:59:09 -0700

    new queries for notes and current chapter

--- wordway/src/lib/db/queries.ts
+++ wordway/src/lib/db/queries.ts
@@ -1,7 +1,16 @@
-import { and, eq } from "drizzle-orm";
+import { and, desc, eq } from "drizzle-orm";
 import { unstable_cache } from "next/cache";
 import { db } from ".";
-import { books, chapters, verses, versions } from "./schema";
+import {
+  books,
+  Chapter,
+  chapters,
+  notes,
+  users,
+  Verse,
+  verses,
+  versions,
+} from "./schema";
 
 const DEFAULT_CACHE_TIME = process.env.NODE_ENV !== "production" ? 1 : 3600;
 
@@ -38,15 +47,75 @@ export const getBooksByVersion = unstable_cache(
   { revalidate: DEFAULT_CACHE_TIME }
 );
 
+export const getChaptersForBook = unstable_cache(
+  async (versionName: string, bookName: string) => {
+    const data = await db
+      .select({
+        id: chapters.id,
+        bookId: chapters.bookId,
+        number: chapters.number,
+        position: chapters.position,
+        createdAt: chapters.createdAt,
+      })
+      .from(chapters)
+      .innerJoin(books, eq(chapters.bookId, books.id))
+      .innerJoin(versions, eq(books.versionId, versions.id))
+      .where(and(eq(versions.name, versionName), eq(books.name, bookName)))
+      .orderBy(chapters.position);
+
+    return data;
+  },
+  [],
+  { revalidate: DEFAULT_CACHE_TIME }
+);
+
+export const getCurrentChapter = unstable_cache(
+  async (
+    versionName: string | undefined,
+    bookName: string | undefined,
+    chapterNumber: string | undefined
+  ) => {
+    if (!versionName || !bookName || !chapterNumber) {
+      return null;
+    }
+
+    const [chapter]: Chapter[] = await db
+      .select({
+        id: chapters.id,
+        bookId: chapters.bookId,
+        number: chapters.number,
+        position: chapters.position,
+        createdAt: chapters.createdAt,
+      })
+      .from(chapters)
+      .innerJoin(books, eq(chapters.bookId, books.id))
+      .innerJoin(versions, eq(books.versionId, versions.id))
+      .where(
+        and(
+          eq(versions.name, versionName),
+          eq(books.name, bookName),
+          eq(chapters.number, chapterNumber)
+        )
+      );
+
+    return chapter;
+  },
+  [],
+  { revalidate: DEFAULT_CACHE_TIME }
+);
+
 export const getVersesForChapter = unstable_cache(
   async (versionName: string, bookName: string, chapterNumber: string) => {
-    const data = await db
+    const data: Verse[] = await db
       .select({
         id: verses.id,
         chapterId: verses.chapterId,
         number: verses.number,
         text: verses.text,
         createdAt: verses.createdAt,
+        version: versions,
+        chapter: chapters,
+        book: books,
       })
       .from(verses)
       .innerJoin(chapters, eq(verses.chapterId, chapters.id))
@@ -66,3 +135,63 @@ export const getVersesForChapter = unstable_cache(
   [],
   { revalidate: DEFAULT_CACHE_TIME }
 );
+
+export const getUserByExternalId = async (externalUserId: string) => {
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.externalUserId, externalUserId));
+
+  if (!user) {
+    throw new Error(`User not found with external id: ${externalUserId}`);
+  }
+
+  return user;
+};
+
+export const insertNote = async (
+  verseId: number,
+  text: string,
+  externalUserId: string
+) => {
+  const user = await getUserByExternalId(externalUserId);
+
+  const [newNote] = await db
+    .insert(notes)
+    .values({
+      userId: user.id,
+      verseId,
+      text,
+    })
+    .returning({ id: notes.id });
+
+  return newNote;
+};
+
+export const getUserNotesForChapter = async (
+  externalUserId: string,
+  chapterId: number
+) => {
+  const user = await getUserByExternalId(externalUserId);
+
+  const userNotesForChapter = await db
+    .select({
+      id: notes.id,
+      text: notes.text,
+      createdAt: notes.createdAt,
+      updatedAt: notes.updatedAt,
+      chapter: chapters,
+      verse: verses,
+      book: books,
+      version: versions,
+    })
+    .from(notes)
+    .innerJoin(verses, eq(notes.verseId, verses.id))
+    .innerJoin(chapters, eq(verses.chapterId, chapterId))
+    .innerJoin(books, eq(books.id, chapters.bookId))
+    .innerJoin(versions, eq(versions.id, books.versionId))
+    .where(and(eq(chapters.id, chapterId), eq(notes.userId, user.id)))
+    .orderBy(desc(notes.updatedAt));
+
+  return userNotesForChapter;
+};
