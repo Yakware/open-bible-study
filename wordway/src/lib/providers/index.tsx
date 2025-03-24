cc6c0a14ab2b94a2a3b3a289569d44d4e05d7f18
devcshort <13677134+devcshort@users.noreply.github.com>
2025-03-23 17:59:33 -0700

    add new providers

--- wordway/src/lib/providers/index.tsx
+++ wordway/src/lib/providers/index.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/modules/common/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { SidebarProvider } from "@/modules/common/components/ui/sidebar";
+import { NotesContextProvider } from "../context/notes-context";
 
 type ProvidersProps = {
   children: ReactNode;
@@ -17,7 +18,9 @@ export function Providers({ children }: ProvidersProps) {
     <NuqsAdapter>
       <SidebarProvider>
         <QueryClientProvider client={queryClient}>
-          <TooltipProvider>{children}</TooltipProvider>
+          <TooltipProvider>
+            <NotesContextProvider>{children}</NotesContextProvider>
+          </TooltipProvider>
         </QueryClientProvider>
       </SidebarProvider>
     </NuqsAdapter>
