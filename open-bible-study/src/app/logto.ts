6109c97db99b4385c4fe86bdf73b9e1dcfa29774
devcshort <13677134+devcshort@users.noreply.github.com>
2025-04-15 16:54:18 -0700

    fix: add additional required user scopes

--- open-bible-study/src/app/logto.ts
+++ open-bible-study/src/app/logto.ts
@@ -1,8 +1,11 @@
-export const logtoConfig = {
+import { UserScope, LogtoNextConfig } from "@logto/next";
+
+export const logtoConfig: LogtoNextConfig = {
   endpoint: process.env.LOGTO_ENDPOINT as string,
   appId: process.env.LOGTO_APP_ID as string,
   appSecret: process.env.LOGTO_APP_SECRET as string,
   baseUrl: process.env.LOGTO_BASE_URL as string,
   cookieSecret: process.env.LOGTO_COOKIE_SECRET as string,
   cookieSecure: process.env.NODE_ENV === "production",
+  scopes: [UserScope.Email, UserScope.Phone],
 };
