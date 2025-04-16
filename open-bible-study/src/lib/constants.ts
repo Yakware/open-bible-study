7520b2fbb1f47bb0102d0e9b496b53439ed3670a
devcshort <13677134+devcshort@users.noreply.github.com>
2025-04-16 15:55:08 -0700

    feat: add umami analytics

--- open-bible-study/src/lib/constants.ts
+++ open-bible-study/src/lib/constants.ts
@@ -1,5 +1,8 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Open Bible Study";
 
+export const UMAMI_SRC = process.env.UMAMI_SRC || "";
+export const UMAMI_WEBSITE_ID = process.env.UMAMI_WEBSITE_ID || "";
+
 export const PASSWORD_PATTERNS = {
   UPPERCASE: /[A-Z]/,
   LOWERCASE: /[a-z]/,
