a2883b32137b31dc79dde828b07a2ce6e37d0916
devcshort <13677134+devcshort@users.noreply.github.com>
2025-04-16 17:01:42 -0700

    feat: create responsive dialog

---
+++ open-bible-study/src/components/responsive-dialog/index.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+type ResponsiveDialogProps = {
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+  trigger: React.ReactNode;
+};
+
+export function ResponsiveDialog({
+  title,
+  description,
+  children,
+  trigger,
+}: ResponsiveDialogProps) {
+  const [open, setOpen] = React.useState(false);
+  const isMobile = useIsMobile();
+
+  if (!isMobile) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>{trigger}</DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{description}</DialogDescription>
+          </DialogHeader>
+          {children}
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{trigger}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>{title}</DrawerTitle>
+          <DrawerDescription>{description}</DrawerDescription>
+        </DrawerHeader>
+        {children}
+      </DrawerContent>
+    </Drawer>
+  );
+}
