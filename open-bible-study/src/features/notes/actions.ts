0c3089541573da2a94d50b78a2efadecaa37c114
devcshort <13677134+devcshort@users.noreply.github.com>
2025-04-17 18:53:06 -0700

    feat: fetch notes

--- open-bible-study/src/features/notes/actions.ts
+++ open-bible-study/src/features/notes/actions.ts
@@ -1,13 +1,27 @@
 "use server";
-
-import { createNote } from "@/data-access/notes";
+import {
+  createNote as createNoteInDb,
+  getNotesByUserIdChapterId,
+} from "@/data-access/notes";
 import { getUserSession } from "@/data-access/users";
 import { Note } from "@/lib/db/schema";
 import { addNoteFormSchema } from "./components/add-note-dialog/add-note-form-schema";
 
-export async function createNoteAction(
-  newNote: Pick<Note, "content" | "verseId">
-) {
+export async function fetchNotes(chapterId: number | undefined) {
+  const { session, user } = await getUserSession();
+
+  if (!session) {
+    throw new Error(`Not logged in`);
+  }
+
+  if (!chapterId) {
+    throw new Error("Invalid data");
+  }
+
+  return getNotesByUserIdChapterId(user.id, chapterId);
+}
+
+export async function createNote(newNote: Pick<Note, "content" | "verseId">) {
   const { session, user } = await getUserSession();
 
   if (!session) {
@@ -20,5 +34,5 @@ export async function createNoteAction(
     throw new Error("Invalid data");
   }
 
-  await createNote({ ...data, userId: user.id });
+  await createNoteInDb({ ...data, userId: user.id });
 }
