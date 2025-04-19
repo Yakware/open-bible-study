9418dbd8d2488e4876589708e2f06e0e1b8393b2
devcshort <13677134+devcshort@users.noreply.github.com>
2025-04-18 19:51:03 -0700

    feat: create actions for updating and deleting notes

--- open-bible-study/src/features/notes/actions.ts
+++ open-bible-study/src/features/notes/actions.ts
@@ -1,11 +1,14 @@
 "use server";
 import {
   createNote as createNoteInDb,
+  updateNote as updateNoteInDb,
+  deleteNote as deleteNoteFromDb,
   getNotesByUserIdChapterId,
 } from "@/data-access/notes";
 import { getUserSession } from "@/data-access/users";
 import { Note } from "@/lib/db/schema";
 import { addNoteFormSchema } from "./components/add-note-dialog/add-note-form-schema";
+import { updateNoteFormSchema } from "./components/chapter-notes-list/update-note-form-schema";
 
 export async function fetchNotes(chapterId: number | undefined) {
   const { session, user } = await getUserSession();
@@ -35,4 +38,30 @@ export async function createNote(newNote: Pick<Note, "content" | "verseId">) {
   }
 
   await createNoteInDb({ ...data, userId: user.id });
+}
+
+export async function updateNote(newNote: Pick<Note, "content" | "id">) {
+  const { session, user } = await getUserSession();
+
+  if (!session) {
+    throw new Error(`Not logged in`);
+  }
+
+  const { error, data } = await updateNoteFormSchema.safeParseAsync(newNote);
+
+  if (error) {
+    throw new Error("Invalid data");
+  }
+
+  await updateNoteInDb(data.id, user.id, data.content);
+}
+
+export async function deleteNote(id: number) {
+  const { session, user } = await getUserSession();
+
+  if (!session) {
+    throw new Error(`Not logged in`);
+  }
+
+  await deleteNoteFromDb(id, user.id);
 }
